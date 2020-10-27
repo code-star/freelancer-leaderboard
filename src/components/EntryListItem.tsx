2ea2fe9fda7fea05113c2f2e9457df2c513d10e7
M.D. van Es <es@mdworld.nl>
2020-10-27 15:31:48 +0100

    Show date

--- src/components/EntryListItem.tsx
+++ src/components/EntryListItem.tsx
@@ -1,5 +1,11 @@
 import React, { FC } from "react";
-import { ListItem, Grid } from "@material-ui/core";
+import {
+  ListItem,
+  Grid,
+  Typography,
+  withStyles,
+  Theme,
+} from "@material-ui/core";
 import { Entry } from "../useLeaderboard";
 
 interface Props {
@@ -7,14 +13,39 @@ interface Props {
   entry: Entry;
 }
 
-const EntryListItem: FC<Props> = ({ index, entry: { name, score } }) => (
+const EntryTypography = withStyles((theme: Theme) => ({
+  body1: {
+    fontWeight: "bold",
+    fontFamily: "Vibrocentric-Regular, serif",
+  },
+  body2: {
+    fontFamily: "Vibrocentric-Regular, serif",
+    fontSize: "70%",
+  },
+}))(Typography);
+
+const formatDate = (input: string) => {
+  const date = new Date(input);
+  const formatted = new Intl.DateTimeFormat("nl-NL", {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  }).format(date);
+  return formatted;
+};
+
+const EntryListItem: FC<Props> = ({ index, entry: { name, date, score } }) => (
   <ListItem>
     <Grid container>
       <Grid item xs={1}>
         {index}
       </Grid>
       <Grid item xs>
-        {name}
+        <EntryTypography>{name}</EntryTypography>
+        <EntryTypography variant="body2">{formatDate(date)}</EntryTypography>
       </Grid>
       <Grid item xs={3}>
         {score}
